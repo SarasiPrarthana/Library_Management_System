1fd95df454f8ba51afcd795d97626e0a84b6ea4f
SarasiPrarthana <prarthanasarasi32@gmail.com>
2025-11-18 15:52:36 +0530

    Implement service layer for adding members

--- src/main/java/org/example/service/MemberService.java
+++ src/main/java/org/example/service/MemberService.java
@@ -1,4 +1,13 @@
 package org.example.service;
 
+import org.example.model.Member;
+import org.example.repository.MemberRepository;
+
 public class MemberService {
+
+    MemberRepository memberRepository = new MemberRepository();
+
+    public void add(Member member){
+        memberRepository.addMember(member);
+    }
 }
