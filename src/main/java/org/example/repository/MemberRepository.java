e7bc088278fe341b08d37237b53079f21129b788
SarasiPrarthana <prarthanasarasi32@gmail.com>
2025-11-19 21:42:39 +0530

    introduce service layer for Member entity and delegate persistence to MemberRepository

--- src/main/java/org/example/repository/MemberRepository.java
+++ src/main/java/org/example/repository/MemberRepository.java
@@ -7,6 +7,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+import java.util.List;
+
 public class MemberRepository {
 
     SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
@@ -18,5 +20,24 @@ public class MemberRepository {
     public void addMember(Member member){
         session.persist(member);
         transaction.commit();
+    }
+
+    public Member getMember(String id) {
+        return session.find(Member.class,id);
+    }
+
+    public void deleteMember(String id) {
+        session.remove(session.find(Member.class,id));
+        transaction.commit();
+    }
+
+    public void updateMember(Member member) {
+        session.merge(member);
+        transaction.commit();
+    }
+
+    public List<Member> getAll() {
+        List<Member> memberList = session.createQuery("FROM Member", Member.class).list();
+        return memberList;
     }
 }
